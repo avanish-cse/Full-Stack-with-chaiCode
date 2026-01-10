21c57b0da66cc5561b3e8df41328e4ba1a41c430
Avanish Chaurasiya <avanishchaurasiya71@gmail.com>
2026-01-10 15:03:49 +0530

    todo basic working

--- front-end/projects/01_todo-localstorage-starter/01_todo-localstorage/script.js
+++ front-end/projects/01_todo-localstorage-starter/01_todo-localstorage/script.js
@@ -1,1 +1,22 @@
+const todoInput = document.querySelector("#todo-input");
+const addTaskBtn = document.querySelector("#add-task-btn");
+const todoList = document.querySelector("#todo-list");
 
+let tasks = [];
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = todoInput.value.trim();
+
+  if (taskText == "") return;
+
+  const newTask = {
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+  todoInput.value = ""
+  console.log(tasks);
+  
+});
